39f8b2c08369e17abfc0d5d13609cc8ad7e13ccd
Lyon Jenkins <lyonjenkins@gmail.com>
2021-10-01 23:12:03 -0500

    Added options for ADMIN and CLAN WL

--- src/plugins/whitelistAPI.js
+++ src/plugins/whitelistAPI.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3005
 import {fetchPlayers} from "../database/controllers/player";
+import { CLAN_WL, ADMIN_WL } from "../../config.json"
 
 
 app.get('/whitelist', (req, res) => {
@@ -12,6 +13,14 @@ app.get('/whitelist', (req, res) => {
         }
         res.send(adminList)
     })
+})
+
+app.get('/adminlist', (req, res) => {
+    res.sendFile(ADMIN_WL)
+})
+
+app.get('/clanwhitelist', (req, res) => {
+    res.sendFile(CLAN_WL)
 })
 
 export default {
